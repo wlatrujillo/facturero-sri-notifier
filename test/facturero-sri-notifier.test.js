1706eef6a3bcfece5479f7751c337d77aa8fc2c2
Wladimir Trujillo <wladimir.trujillo@outlook.com>
2026-02-10 10:15:18 -0500

    feat: init project

---
+++ test/facturero-sri-notifier.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib/core');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const FactureroSriNotifier = require('../lib/facturero-sri-notifier-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/facturero-sri-notifier-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new FactureroSriNotifier.FactureroSriNotifierStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
